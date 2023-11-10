284bbb34cb9426a2bbae46942e3cc2016cb09b7b
AbdellahNassim <abdallah.nassim.meridja@gmail.com>
2023-11-10 18:26:29 +0100

    feat: fixed main height

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,13 +1,12 @@
-import Image from 'next/image'
-import { Inter, Rubik } from '@next/font/google'
-import styles from './page.module.css'
-import { previewData } from 'next/headers'
-import { client } from "../../../lib/sanity.client"
-import { groq } from 'next-sanity'
-import PreviewSuspense from "../../components/PreviewSuspense"
-import PreviewBlogList from '@/components/PreviewBlogList'
-import BlogList from '@/components/BlogList'
-
+import Image from "next/image";
+import { Inter, Rubik } from "@next/font/google";
+import styles from "./page.module.css";
+import { previewData } from "next/headers";
+import { client } from "../../../lib/sanity.client";
+import { groq } from "next-sanity";
+import PreviewSuspense from "../../components/PreviewSuspense";
+import PreviewBlogList from "@/components/PreviewBlogList";
+import BlogList from "@/components/BlogList";
 
 const query = groq`
 *[_type == "post"] {
@@ -15,25 +14,30 @@ const query = groq`
   author->,
   categories[]->
 } | order(_createdAt desc)
-`
+`;
 
 //const rubik = Rubik({ subsets: ['latin'] })
 
-export const revalidate = 60
+export const revalidate = 60;
 
 export default async function Home() {
-
   if (previewData()) {
     return (
-      <PreviewSuspense fallback={<div className='text-center text-lg animate-pulse text-[#E7E247]'>Loading Preview</div> }>
-          <PreviewBlogList query={query} />
+      <PreviewSuspense
+        fallback={
+          <div className="animate-pulse text-center text-lg text-[#E7E247]">
+            Loading Preview
+          </div>
+        }
+      >
+        <PreviewBlogList query={query} />
       </PreviewSuspense>
-    )
+    );
   }
-  const posts = await client.fetch(query)
+  const posts = await client.fetch(query);
   return (
-    <main >
-      <BlogList posts={posts}/>
+    <main className="min-h-screen">
+      <BlogList posts={posts} />
     </main>
-  )
+  );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,21 +1,28 @@
-import { Dancing_Script } from "@next/font/google"
-type Props = {}
-const dancing_script = Dancing_Script({ subsets: ['latin'] })
+import { Dancing_Script } from "@next/font/google";
+type Props = {};
+const dancing_script = Dancing_Script({ subsets: ["latin"] });
 function Banner({}: Props) {
   return (
-    <div className="mt-10 md:mt-5 flex text-center flex-col md:flex-row md:text-left items-center justify-between space-x-2 px-10 py-2">
-        <div>
-            <h1 className={`${dancing_script.className} text-7xl font-bold`}>Inflow Daily Blog</h1>
-             <h2 className="mt-8 md:mt-4 text-2xl">
-              Welcome to {" "}
-              <span className="underline decoration-4 decoration-[#E7E247] font-bold">Every Technology Enthusiast</span>
-             </h2>
-        </div>
-        <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
-            <span className={`${dancing_script.className} text-xl font-bold`}>Inflow</span> the right place to get your daily dose of technical knowledge
-        </p>
+    <div className="mt-10 flex flex-col items-center justify-between space-x-2 px-10 py-2 text-center md:mt-5 md:flex-row md:text-left">
+      <div>
+        <h1 className={`${dancing_script.className} text-7xl font-bold`}>
+          Inflow Weekly Blog
+        </h1>
+        <h2 className="mt-8 text-2xl md:mt-4">
+          Welcome to{" "}
+          <span className="font-bold underline decoration-[#E7E247] decoration-4">
+            Every Technology Enthusiast
+          </span>
+        </h2>
+      </div>
+      <p className="mt-5 max-w-sm text-gray-400 md:mt-2">
+        <span className={`${dancing_script.className} text-xl font-bold`}>
+          Inflow
+        </span>{" "}
+        the right place to get your weekly dose of technical knowledge
+      </p>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
