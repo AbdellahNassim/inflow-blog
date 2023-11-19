8ec7e8795fb55c9e1bf9f449268ab23f69cfcf2b
AbdellahNassim <abdallah.nassim.meridja@gmail.com>
2023-11-19 21:22:46 +0100

    migrated to firebase for the messages

--- src/components/PreviewBlogList.tsx
+++ src/components/PreviewBlogList.tsx
@@ -2,15 +2,13 @@
 import { usePreview } from "lib/sanity.preview";
 import BlogList from "./BlogList";
 type Props = {
-    query: string
-}
+  query: string;
+};
 
 function PreviewBlogList({ query }: Props) {
-    const posts = usePreview(null, query)
-    //console.log("LOADING POSTS", posts)
-    return (
-        <BlogList posts={posts} />
-    )
+  const posts = usePreview(null, query);
+  //console.log("LOADING POSTS", posts)
+  return <BlogList posts={posts} searchParams={{}} />;
 }
 
-export default PreviewBlogList
+export default PreviewBlogList;
--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,38 +1,54 @@
-import { sendEmail} from "@/util/mail";
-import type { SendError } from "@/util/mail";
+// import { sendEmail } from "@/util/mail";
+// import type { SendError } from "@/util/mail";
 import { NextApiHandler } from "next";
 import { withHCaptcha } from "next-hcaptcha";
-
-
+import { firestore } from "@/firebase/firestore";
+import { serverTimestamp } from "firebase/firestore";
 const handler: NextApiHandler = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: "Invalid method" });
+  }
 
-    if (req.method !== 'POST') {
-        return res.status(400).json({ error: 'Invalid method' });
-    }
+  const { name, email, message, subject } = req.body;
+  //console.log(name,email,message,subject)
+  if (!name || !email || !message || !subject) {
+    return res.status(400).json({ error: "Missing some fields" });
+  }
 
-    const { name, email, message, subject } = req.body;
-    //console.log(name,email,message,subject)
-    if (!name || !email || !message || !subject) {
-       
-        return res.status(400).json({ error: 'Missing some fields' });
-    }
-    /// send email with sendgrid
-    try {
-        await sendEmail(
-            email,
-            `[Inflow Blog Contact] ${subject}`,
-            `<div>
-                ${message}
-            </div>`
-        );
-    }catch (err) {
-        console.log((err as SendError).response)
-        return res.status(400).redirect("/contact?error=sendgrid");
-    }
-    res.status(200).redirect("/contact/submissionSuccess")
+  try {
+    const { name, email, message } = req.body; // Assuming these are the fields in your form
 
-}
+    // Save to Firestore
+    await firestore.collection("messages").add({
+      name,
+      email,
+      message,
+      subject,
+      timestamp: new Date().toISOString(),
+    });
+    res.redirect(302, "/contact/submissionSuccess");
+  } catch (error) {
+    console.error("Error saving message to Firestore", error);
+    res.status(500).json({ error: "Error sending message" });
+  }
+  //   /// send email with sendgrid
+  //   try {
+  //     await sendEmail(
+  //       email,
+  //       `[Inflow Blog Contact] ${subject}`,
+  //       `<div>
+  //                 ${message}
+  //             </div>`
+  //     );
+  //   } catch (err) {
+  //     console.log((err as SendError).response);
+  //     return res.redirect(400, "/contact?error=sendgrid");
+  //   }
+  //   res.redirect(302, "/contact/submissionSuccess");
+};
 
-export default withHCaptcha(handler, {envVarNames : {
-    secret : "HCAPTCHA_SECRET",
-}})
+export default withHCaptcha(handler, {
+  envVarNames: {
+    secret: "HCAPTCHA_SECRET",
+  },
+});
